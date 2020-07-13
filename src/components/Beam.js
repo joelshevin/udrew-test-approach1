da64ec1887ffe24ed8b417a84a965060dd431794
Joel Shevin <joelshevinrog@gmail.com>
2020-07-13 21:33:03 +0800

    initial commit

---
+++ src/components/Beam.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Slider } from "@rmwc/slider";
+import "@rmwc/slider/styles";
+
+export const Beam = () => {
+  const [height, setHeight] = useState(50);
+  const [width, setWidth] = useState(100);
+
+  //get the value from the slider event to set as height
+  const heighInput = (event) => {
+    setHeight(event.detail.value);
+  };
+
+  //get the value from the slider event to set as width
+  const widthInput = (event) => {
+    setWidth(event.detail.value);
+  };
+
+  return (
+    <div>
+      <div class="row">
+        <div class="column">
+          <h3>Height</h3>
+        </div>
+        <div class="column" style={{ width: "30%" }}>
+          <Slider discrete="true" min={50} max={150} onInput={heighInput} />
+        </div>
+      </div>
+      <div class="row">
+        <div class="column">
+          <h3>Width</h3>
+        </div>
+        <div class="column" style={{ width: "30%" }}>
+          <Slider discrete="true" min={100} max={300} onInput={widthInput} />
+        </div>
+      </div>
+      <div class="row">
+        <div class="singleColumn">
+          <svg height="300" width="300">
+            <rect
+              width={width}
+              height={height}
+              stroke="black"
+              stroke-width="1"
+              fill="teal"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
