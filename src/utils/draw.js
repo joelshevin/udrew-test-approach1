02cf13521351da92cfe6fcaf32ba87800d94f801
Joel Shevin <joelshevin@Unknown.gateway>
2020-07-21 15:19:36 +0800

    updates

--- src/utils/draw.js
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from "react";
-import { DimensionContext } from "../Carport";
-
-export const Draw = (length, width) => {
-  return {
-    length: length,
-    width: width,
-  };
-};
