fee6d07492aa99d22d78479f3a2f637c476edbe4
Joel Shevin <joelshevin@Joels-Air.modem>
2020-07-24 15:14:53 +0800

    Updated material

--- src/components/Material.js
+++ src/components/Material.js
@@ -1,9 +1,16 @@
 import React from "react";
+import styles from "./Material.module.css";
 
 export const Material = ({ width, length }) => {
   return (
     <>
-      <svg x="0" y="0" height={length / 40} width={width / 20}>
+      <svg
+        className={styles.img}
+        x="0"
+        y="0"
+        height={length / 40}
+        width={width / 20}
+      >
         <image href="https://i.pinimg.com/600x315/54/40/99/544099fae290ce3e658a165d43151f9c.jpg" />
       </svg>
     </>
