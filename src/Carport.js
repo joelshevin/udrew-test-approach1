635f04126ba134a7fea25ff0b9ca48fa4c548531
Joel Shevin <joelshevin@Joels-Air.modem>
2020-07-24 15:53:16 +0800

    updated mm unit in sliders

--- src/Carport.js
+++ src/Carport.js
@@ -37,7 +37,7 @@ export const Carport = () => {
         </div>
         <div class="column" className={styles.sliders}>
           <Slider discrete="true" min={5850} max={8000} onInput={lengthInput} />
-          {length}
+          {length}mm
         </div>
       </div>
       <div class="row">
@@ -46,7 +46,7 @@ export const Carport = () => {
         </div>
         <div class="column" className={styles.sliders}>
           <Slider discrete="true" min={3500} max={6500} onInput={widthInput} />
-          {width}
+          {width}mm
         </div>
         <div class="column" className={styles.sliders}></div>
       </div>
